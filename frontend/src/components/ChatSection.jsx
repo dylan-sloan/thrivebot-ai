b80cef09ae417208afe283da805fa63156932b7c
Dylan Sloan <dylansloanage@gmail.com>
2024-11-17 22:10:52 -0500

    Delete unused logo and change placeholder text to be more relevant

--- frontend/src/components/ChatSection.jsx
+++ frontend/src/components/ChatSection.jsx
@@ -4,7 +4,7 @@ function ChatSection() {
   return (
     <div className="chat-section">
       <div className="message-area">Area for current message</div>
-      <input type="text" placeholder="Type Here: placeholder" className="input-box" />
+      <input type="text" placeholder="Enter something here" className="input-box" />
       <div className="navigation-buttons">
         <button>Settings</button>
         <button>Previous</button>
