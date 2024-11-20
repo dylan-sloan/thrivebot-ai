5166240f32f4b8fe23a81db8d2361427305dc861
James Perdue <65421746+JTPerdue525@users.noreply.github.com>
2024-11-20 18:29:22 -0500

    Added functions to set the user's message equal to the input.

--- frontend/src/components/ChatSection.jsx
+++ frontend/src/components/ChatSection.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 function ChatSection() {
+  const [message, setMessage] = useState('Area for next message');
+  const [input, setInput] = useState('')
+
+  function _handleInputChange(event){ //Allows for real time changes in input
+    setInput(event.target.value);
+  }
+
+  function _handleKeyDown(event){ //Sets the message section equal to the current input and clears input
+    if(event.key === 'Enter'){
+      setMessage(input);
+      setInput('Enter your message here');
+    }
+  }
+
   return (
     <div className="chat-section">
-      <div className="message-area">Area for current message</div>
-      <input type="text" placeholder="Enter something here" className="input-box" />
+      <div className="message-area">{message}</div>
+      <input type="text" placeholder='Enter your message here' value={input} className="input-box" onChange={_handleInputChange} onKeyDown={_handleKeyDown} />
       <div className="navigation-buttons">
         <button>Settings</button>
         <button>Previous</button>
