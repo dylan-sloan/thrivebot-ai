146452e2f99c1e46b67de95c028512cf72e96960
Dylan Sloan <dylansloanage@gmail.com>
2024-11-20 20:01:33 -0500

    Allow for multiple messages and reset text field after a message is sent

--- frontend/src/components/ChatSection.jsx
+++ frontend/src/components/ChatSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 function ChatSection() {
-  const [message, setMessage] = useState('Area for next message');
+  const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('')
 
   function _handleInputChange(event){ //Allows for real time changes in input
@@ -10,20 +10,26 @@ function ChatSection() {
   }
 
   function _handleKeyDown(event){ //Sets the message section equal to the current input and clears input
-    if(event.key === 'Enter'){
-      setMessage(input);
-      setInput('Enter your message here');
+    if(event.key === 'Enter' && input.trim() !== ''){
+      setMessages(prevMessages => [...prevMessages, input])
+      setInput(''); // reset input
     }
   }
 
   return (
     <div className="chat-section">
-      <div className="message-area">{message}</div>
+      <div className="message-area">
+        {messages.map((msg, msg_id) => (
+        <p key={msg_id}>{msg}</p>
+      ))}
+      </div>
       <input type="text" placeholder='Enter your message here' value={input} className="input-box" onChange={_handleInputChange} onKeyDown={_handleKeyDown} />
       <div className="navigation-buttons">
         <button>Settings</button>
-        <button>Previous</button>
-        <button>Next</button>
+        <div className="prev-next-buttons">
+          <button>Previous</button>
+          <button>Next</button>
+        </div>
       </div>
     </div>
   );
